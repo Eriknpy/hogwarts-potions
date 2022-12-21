d5cc74160f4d7d20b3acb83168f8bb264757e95e
Erik <101416162+Eriknpy@users.noreply.github.com>
2022-12-21 10:38:28 +0100

    create : PotionController

---
+++ Controllers/PotionController.cs
@@ -0,0 +1,24 @@
+﻿using HogwartsPotions.Data.Services;
+using HogwartsPotions.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HogwartsPotions.Controllers
+{
+    [ApiController, Route("/potion")]
+    public class PotionController : Controller
+    {
+        private readonly IPotionService _service;
+        public PotionController(IPotionService context)
+        {
+            _service = context;
+        }
+
+        [HttpGet("all")]
+        public async Task<List<Potion>> GetAllPotions()
+        {
+            return await _service.GetAllPotions();
+        }
+    }
+}
