db419c12f776bd6efd787b0809b28d7963d9b8ea
Erik <101416162+Eriknpy@users.noreply.github.com>
2022-12-21 21:35:13 +0100

    add : Methods
    + IsSpaceLeft
    + SetRoomCapacity
    + UpdateCapacity

--- Models/Entities/Room.cs
+++ Models/Entities/Room.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -9,10 +10,31 @@ namespace HogwartsPotions.Models.Entities
         public long ID { get; set; }
         public int Capacity { get; set; }
         public HashSet<Student> Residents { get; set; }
-
         public Room()
         {
             Residents = new HashSet<Student>();
+            SetRoomCapacity();
+        }
+
+        public bool IsSpaceLeft()
+        {
+            int maxRoomCapacity = 4;
+            return Capacity < maxRoomCapacity;
+        }
+
+        private void SetRoomCapacity()
+        {
+            Capacity = Residents.Count;
+        }
+
+        public void UpdateCapacity(HashSet<Student> students)
+        {
+            int maxRoomCapacity = 4;
+            if (IsSpaceLeft() && (maxRoomCapacity-Capacity >= students.Count))
+            {
+                Capacity = students.Count;
+            }
+            throw new Exception("The room is full!");
         }
     }
 }
