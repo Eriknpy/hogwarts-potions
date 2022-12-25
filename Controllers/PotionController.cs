2bd8930b1f7e5963905443ffec26fffc4c61efd3
Erik <101416162+Eriknpy@users.noreply.github.com>
2022-12-25 15:01:42 +0100

    add : Potion endpoints
    + AddPotion
    + GetAllPotionsByStudent
    + GetStudentById
    + BrewPotion

--- Controllers/PotionController.cs
+++ Controllers/PotionController.cs
@@ -26,5 +26,51 @@ namespace HogwartsPotions.Controllers
             }
             return Ok(potions);
         }
+
+        [HttpPost]
+        public async Task<ActionResult> AddPotion([FromBody] Potion potion)
+        {
+            Student student = await _service.GetStudent(potion.Brewer.Id);
+            if (student is null)
+            {
+                return NotFound($"Student with ID of {potion.Brewer.Id} doesn't exist!");
+            }
+
+            await _service.AddPotion(potion);
+            return Created("New Potion Added", potion);
+        }
+
+        [HttpGet("{studentId:long}")]
+        public async Task<ActionResult> GetAllPotionsByStudent(long studentId)
+        {
+            var studentPotions = await _service.GetAllPotionsByStudent(studentId);
+            if (studentPotions == null)
+            {
+                return NotFound();
+            }
+
+            if (studentPotions.Count == 0)
+            {
+                return NotFound($"{studentId}. Student doesn't have any potions yet!");
+            }
+            return Ok(studentPotions);
+        }
+
+        [HttpGet("student/{studentId:long}")]
+        public async Task<ActionResult> GetStudentById(long studentId)
+        {
+            var student = await _service.GetStudent(studentId);
+            if (student is null)
+            {
+                return NotFound($"Student #{studentId} doesn't exist!");
+            }
+            return Ok(student);
+        }
+
+        [HttpPost("brew/{studentId:long}")]
+        public async Task<ActionResult> BrewPotion()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
