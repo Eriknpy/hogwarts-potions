f085fbfb40d5f7c164d212921552d5a4b5bb2f99
Erik <101416162+Eriknpy@users.noreply.github.com>
2023-01-17 19:00:26 +0100

    update : Dbcontext OnModelCreating

--- Data/HogwartsContext.cs
+++ Data/HogwartsContext.cs
@@ -14,6 +14,11 @@ namespace HogwartsPotions.Data
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Room>().ToTable("Room");
+            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Ingredient>().ToTable("Ingredient");
+            modelBuilder.Entity<Recipe>().ToTable("Recipe");
+            modelBuilder.Entity<Potion>().ToTable("Potion");
         }
         public DbSet<Room> Rooms { get; set; }
         public DbSet<Student> Students { get; set; }
